84de3a384bc29ea968af35fcc50ff6e42ef82da5
Дмитрий Борисенко <borisenko-dmitriyy2014@yandex.ru>
2021-03-27 07:28:29 +0300

    First

---
+++ src/chat/db/ConnectionDB.java
@@ -0,0 +1,32 @@
+package chat.db;
+
+import java.sql.*;
+
+public class ConnectionDB {
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "Dima10910000";
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    private static Statement statement;
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            System.out.println("Connection successful");
+        }catch (SQLException exc){
+            exc.printStackTrace(System.out);
+        }
+    }
+    static {
+        try{
+            statement = connection.createStatement();
+            System.out.println("Statement successful");
+        }catch (SQLException exc){
+            exc.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
